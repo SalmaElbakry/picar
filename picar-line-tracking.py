fb829231d6535bd97983250867d7405d62ef11a1
Salma Elbakry <79708975+SalmaElbakry@users.noreply.github.com>
2024-11-07 15:51:33 +0300

    Update picar-line-tracking.py

--- picar-line-tracking.py
+++ picar-line-tracking.py
@@ -14,7 +14,7 @@ from board import SCL, SDA
 import busio
 # Import the PCA9685 module.
 from adafruit_pca9685 import PCA9685
-
+from controller import Robot, Camera, Motor
 # Create the I2C bus interface.
 i2c_bus = busio.I2C(SCL, SDA)
 
@@ -22,152 +22,45 @@ GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
 pwm =  PCA9685(i2c_bus)
 
-# Set frequency to 50hz, good for servos.
-pwm.frequency = 50
+# Initialization
+robot = Robot()
+timestep = int(robot.getBasicTimeStep())
 
-# Alternatively specify a different address and/or bus:
-high_speed = 0x7FFF  # Max pulse length out of 4096
-mid_speed = 0x4FFF  
-low_speed = 0x2FFF   
+# Set up the camera and motors
+camera = robot.getDevice("camera")
+camera.enable(timestep)
+width = camera.getWidth()
+height = camera.getHeight()
 
-short_delay=0.1
-long_delay=0.2
-extra_long_delay=0.3
+left_motor = robot.getDevice("left wheel motor")
+right_motor = robot.getDevice("right wheel motor")
 
-GPIO.setmode(GPIO.BCM) # GPIO number  in BCM mode
-GPIO.setwarnings(False)
-#define L298N(Model-Pi motor drive board) GPIO pins
-IN1 = 23  #left motor direction pin
-IN2 = 24  #left motor direction pin
-IN3 = 27  #right motor direction pin
-IN4 = 22  #right motor direction pin
-ENA = 0  #left motor speed PCA9685 port 0
-ENB = 1  #right motor speed PCA9685 port 1
-sensor1= 5 # No.1 sensor from far left
-sensor2= 6 # No.2 sensor from left
-sensor3= 13 # middle sensor
-sensor4= 19 # No.2 sensor from right
-sensor5= 26 #No.1 sensor from far  right
-sts1=0
-sts2=0
-sts3=0
-sts4=0
-sts5=0
+# Set motors to velocity mode
+left_motor.setPosition(float('inf'))
+right_motor.setPosition(float('inf'))
 
-# Define motor control  pins as output
-GPIO.setup(IN1, GPIO.OUT)   
-GPIO.setup(IN2, GPIO.OUT) 
-GPIO.setup(IN3, GPIO.OUT)   
-GPIO.setup(IN4, GPIO.OUT) 
-GPIO.setup(sensor1, GPIO.IN)   
-GPIO.setup(sensor2, GPIO.IN)
-GPIO.setup(sensor3, GPIO.IN)   
-GPIO.setup(sensor4, GPIO.IN)
-GPIO.setup(sensor5, GPIO.IN)  
+# Base speed setting
+base_speed = 3.0
+turn_speed = 1.5
 
-def changespeed(left_speed,right_speed):
-	pwm.channels[ENB].duty_cycle = left_speed
-	pwm.channels[ENA].duty_cycle = right_speed
+# Main control loop
+while robot.step(timestep) != -1:
+    # Get the image
+    image = camera.getImage()
+    # Extract gray values of the middle row
+    mid_line_gray_values = [camera.imageGetGray(image, width, x, int(height / 2)) for x in range(width)]
 
-def stopcar():
-	GPIO.output(IN1, GPIO.LOW)
-	GPIO.output(IN2, GPIO.LOW)
-	GPIO.output(IN3, GPIO.LOW)
-	GPIO.output(IN4, GPIO.LOW)
-	changespeed(0,0)
-stopcar()
+    # Calculate the average gray value for the left and right halves of the image
+    left_gray = sum(mid_line_gray_values[:width // 2]) / (width // 2)
+    right_gray = sum(mid_line_gray_values[width // 2:]) / (width // 2)
 
-def backward(speed_left,speed_right):
-	GPIO.output(IN1, GPIO.HIGH)
-	GPIO.output(IN2, GPIO.LOW)
-	GPIO.output(IN3, GPIO.HIGH)
-	GPIO.output(IN4, GPIO.LOW)
-	changespeed(speed_left,speed_right)
- 
-	#following two lines can be removed if you want car make continuous movement without pause
-	#time.sleep(short_delay) 
-	#stopcar()
-	
-def forward(speed_left,speed_right):
-	GPIO.output(IN2, GPIO.HIGH)
-	GPIO.output(IN1, GPIO.LOW)
-	GPIO.output(IN4, GPIO.HIGH)
-	GPIO.output(IN3, GPIO.LOW)
-	changespeed(speed_left,speed_right)
-	#following two lines can be removed if you want car make continuous movement without pause
-	#time.sleep(short_delay) 
-	#stopcar()
-	
-def turnRight(speed_left,speed_right):
-	GPIO.output(IN1, GPIO.LOW)
-	GPIO.output(IN2, GPIO.HIGH)
-	GPIO.output(IN3, GPIO.HIGH)
-	GPIO.output(IN4, GPIO.LOW)
-	changespeed(speed_left,speed_right)
-	#following two lines can be removed if you want car make continuous movement without pause
-	#time.sleep(short_delay) 
-	#stopcar()
-	
-def turnLeft(speed_left,speed_right):
-	GPIO.output(IN1, GPIO.HIGH)
-	GPIO.output(IN2, GPIO.LOW)
-	GPIO.output(IN3, GPIO.LOW)
-	GPIO.output(IN4, GPIO.HIGH)
-	changespeed(speed_left,speed_right)	
-	#following two lines can be removed if you want car make continuous movement without pause
-	#time.sleep(short_delay) 
-	#stopcar()
-print('start tracking, press Ctrl-C to quit...')
-
-try:
-	while True :
-
-		sts1 =  0 if GPIO.input(sensor1) else 1
-		sts2 =  0 if GPIO.input(sensor2) else 1
-		sts3 =  0 if GPIO.input(sensor3) else 1
-		sts4 =  0 if GPIO.input(sensor4) else 1
-		sts5 =  0 if GPIO.input(sensor5) else 1
-
-
-		sensorval = ''.join([str(sts1), str(sts2), str(sts3), str(sts4), str(sts5)])
-		print(sensorval)
-
-		if  sensorval=="10000"  or sensorval=="01000" or sensorval=="11000":
-			turnLeft(low_speed,mid_speed)   #The black line left, sharp left turn
-			#time.sleep(long_delay)  
-			#stopcar()
-			#time.sleep(short_delay) 
-		
-		if  sensorval=="01100"  or sensorval=="11100"  or sensorval=="11110" :
-			turnLeft(0,high_speed)   #The black line left,  left turn
-			#time.sleep(long_delay)  
-			#stopcar()
-			#time.sleep(short_delay) 	
-			
-		if sensorval=="00001"  or sensorval=="00010" or sensorval=="00011":
-			turnRight(mid_speed,low_speed) #The black line is  on the Left of the car, need  Left turn 
-			print("right turn")
-			#time.sleep(long_delay)  
-			#stopcar()
-			#time.sleep(short_delay)	
-		
-		if  sensorval=="00110" or  sensorval=="00111" or sensorval=="01111":
-			forward(mid_speed,low_speed) #slight right turn
-			#time.sleep(long_delay)  
-			#stopcar()
-			#time.sleep(short_delay)	
-
-		if  sensorval=="00100"  or sensorval=="01110":
-			forward(mid_speed,mid_speed) #slight right turn
-			#time.sleep(long_delay)  
-			#stopcar()
-			#time.sleep(short_delay) 
-
-		
-		if sensorval=="11111" :
-			stopcar() #The car front touch stop line, need stop
- 
-except KeyboardInterrupt:
-  # User pressed CTRL-C
-  # Reset GPIO settings
-  GPIO.cleanup()
+    # Line-following control logic
+    if left_gray < right_gray:  # Line is more on the left side
+        left_motor.setVelocity(base_speed - turn_speed)
+        right_motor.setVelocity(base_speed + turn_speed)
+    elif right_gray < left_gray:  # Line is more on the right side
+        left_motor.setVelocity(base_speed + turn_speed)
+        right_motor.setVelocity(base_speed - turn_speed)
+    else:  # Line is centered
+        left_motor.setVelocity(base_speed)
+        right_motor.setVelocity(base_speed)
